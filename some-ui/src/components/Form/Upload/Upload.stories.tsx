618c66ebc3b27f4c5dc130c799ad492a9262e8f4
Arslanoov <rasularslanoov@gmail.com>
2022-05-23 12:26:17 +0500

    docs(components): Added form upload stories

---
+++ some-ui/src/components/Form/Upload/Upload.stories.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import Upload from '.';
+
+export default {
+  title: 'Form/Upload',
+  component: Upload,
+} as ComponentMeta<typeof Upload>;
+
+const Template: ComponentStory<typeof Upload> = (args) => <Upload {...args} />;
+
+export const Standard = Template.bind({});
+Standard.args = {};
+
+export const MultipleFiles = Template.bind({});
+MultipleFiles.args = {
+  multiple: true,
+};
+
+export const Disabled = Template.bind({});
+Disabled.args = {
+  disabled: true,
+};
+
+export const WithLabel = Template.bind({});
+WithLabel.args = {
+  label: 'Some label',
+};
